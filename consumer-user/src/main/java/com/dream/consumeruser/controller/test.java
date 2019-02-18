f27421fa768ad4966e66bac0506a6e2a37881bcf
ToDreamYY <16601716505@163.com>
2019-02-18 14:27:30 +0800

    第二次提交

--- consumer-user/src/main/java/com/dream/consumeruser/controller/test.java
+++ consumer-user/src/main/java/com/dream/consumeruser/controller/test.java
@@ -3,6 +3,7 @@ package com.dream.consumeruser.controller;
 public class test {
     @Override
     public boolean equals(Object obj) {
+        System.out.println(1233);
         return super.equals(obj);
     }
 }
