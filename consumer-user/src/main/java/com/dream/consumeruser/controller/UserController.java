18dd734379d15c9c54501c6c2b5b7b674e203393
ToDreamYY <16601716505@163.com>
2019-02-18 14:01:45 +0800

    first test

---
+++ consumer-user/src/main/java/com/dream/consumeruser/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.dream.consumeruser.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class UserController {
+    @Autowired
+    RestTemplate restTemplate;
+    @GetMapping("/buy")
+    public String buyTicket(){
+        String s = restTemplate.getForObject("http://PROVIDER-TICKET/ticket", String.class);
+        return "购买了"+s;
+    }
+}
